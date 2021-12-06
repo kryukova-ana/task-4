b8f979ac3367d1bf7e5958f968c0f39a8f3e5f77
kryukova-ana <morinoshika13@gmail.com>
2021-12-06 17:03:41 +0300

    apd 06.12

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,8 @@
-function showPass() {
-    var y = document.getElementById("newImg");
-
-    var x = document.getElementById("user-password");
-    if (x.type === "password") {
-        x.type = "text";
-        y.src ='../../assets/img/open-eye.svg';
-    } else {
-        x.type = "password";
-        y.src= "../../assets/img/eye-off.svg";
-    }
+function changeTheme() {
+    const btn = document.querySelector('.banner-btn');
+// Отслеживаем щелчок по кнопке
+    btn.addEventListener('click', function() {
+        // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
+        document.body.classList.toggle('body-dark');
+    })
 }
