27340eca2fdd0b18849886ec7b729bf1a0c33c04
kryukova-ana <morinoshika13@gmail.com>
2021-12-14 17:28:25 +0300

    apd 14.12 add dark-theme, but it's don't work lol

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,9 @@
-function changeTheme() {
-    const btn = document.querySelector('.banner-btn');
-// Отслеживаем щелчок по кнопке
-    btn.addEventListener('click', function() {
-        // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-        document.body.classList.toggle('body-dark');
-    })
-}
+const btn = document.querySelector(".banner-btn");
+const theme = document.querySelector("#theme-link");
+btn.addEventListener("click", function() {
+    if (theme.getAttribute("href") == "@@webRoot/assets/css/main.min.css?v21022020") {
+        theme.href = "@@webRoot/assets/css/main-dark.min.css?v21022020";
+    } else {
+        theme.href = "@@webRoot/assets/css/main.min.css?v21022020";
+    }
+});
