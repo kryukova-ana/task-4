2db5070026e97be67fd5be0668151e026d3d3106
kryukova-ana <morinoshika13@gmail.com>
2021-12-15 17:36:50 +0300

    apd 15.12 it's alive!

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,13 @@
-const btn = document.querySelector(".banner-btn");
-const theme = document.querySelector("#theme-link");
-btn.addEventListener("click", function() {
-    if (theme.getAttribute("href") == "@@webRoot/assets/css/main.min.css?v21022020") {
-        theme.href = "@@webRoot/assets/css/main-dark.min.css?v21022020";
-    } else {
-        theme.href = "@@webRoot/assets/css/main.min.css?v21022020";
-    }
-});
+window.onload = function (){
+    let btn = document.querySelector("button");
+    console.log('test',btn);
+    const theme = document.querySelector("#light");
+    btn.addEventListener("click", function () {
+        if (theme.getAttribute("href") == "@@webRoot/assets/css/main.min.css?v21022020") {
+            theme.href = "@@webRoot/assets/css/main-dark.min.css?v21022021";
+        } else {
+            theme.href = "@@webRoot/assets/css/main.min.css?v21022020";
+        }
+    });
+
+}
