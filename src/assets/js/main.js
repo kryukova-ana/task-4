61957763bab76a2d829ac951aabcfeadbaf28215
kryukova-ana <morinoshika13@gmail.com>
2021-12-17 17:01:26 +0300

    apd 17.12 fix: btn - dark-theme

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,14 @@
 window.onload = function (){
-    let btn = document.querySelector("button");
-    console.log('test',btn);
+    let img = document.querySelector(".theme-img");
+    console.log('test',img);
     const theme = document.querySelector("#light");
-    btn.addEventListener("click", function () {
+    img.addEventListener("click", function () {
         if (theme.getAttribute("href") == "/assets/css/main.css") {
             theme.href = "/assets/css/main-dark.css";
+            img.src="/assets/img/dark.png";
         } else {
             theme.href = "/assets/css/main.css";
+            img.src="/assets/img/light.png";
         }
     });
 
