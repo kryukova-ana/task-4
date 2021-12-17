7a124c0adb3b365488a53cf9590c5a0dae105789
kryukova-ana <morinoshika13@gmail.com>
2021-12-17 13:54:49 +0300

    apd 17.12 fix: btn - dark-theme

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,10 +3,10 @@ window.onload = function (){
     console.log('test',btn);
     const theme = document.querySelector("#light");
     btn.addEventListener("click", function () {
-        if (theme.getAttribute("href") == "@@webRoot/assets/css/main.min.css?v21022020") {
-            theme.href = "@@webRoot/assets/css/main-dark.min.css?v21022021";
+        if (theme.getAttribute("href") == "/assets/css/main.css") {
+            theme.href = "/assets/css/main-dark.css";
         } else {
-            theme.href = "@@webRoot/assets/css/main.min.css?v21022020";
+            theme.href = "/assets/css/main.css";
         }
     });
 
